ffda9399579d79e85ce3ea9380d73e9d623d3cff
bartelRW <bartel@rasche-wessler.de>
2025-12-16 09:43:06 +0100

    :tada: Initial Commit

---
+++ src/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace StandaloneWebApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new MainPage()) { Title = "StandaloneWebApp" };
+        }
+    }
+}
---
+++ src/wwwroot/js/chartHelper.js
@@ -0,0 +1,5 @@
+﻿// JavaScript function to assist with rendering charts using Chart.js
+window.renderChart = (canvasId, chartConfig) => {
+    const ctx = document.getElementById(canvasId).getContext('2d');
+    new Chart(ctx, chartConfig);
+};
