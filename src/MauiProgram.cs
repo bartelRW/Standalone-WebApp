02666e84a958ca5307c7b355b2483ca7afc8c175
bartelRW <bartel@rasche-wessler.de>
2026-01-06 08:12:23 +0100

    :bookmark: Release debug packages
    Release App as package with sideloading certificate for debugging purposes

--- src/MauiProgram.cs
+++ src/MauiProgram.cs
@@ -17,8 +17,8 @@ namespace StandaloneWebApp
             builder.Services.AddMauiBlazorWebView();
 
 #if DEBUG
-    		builder.Services.AddBlazorWebViewDeveloperTools();
-    		builder.Logging.AddDebug();
+            builder.Services.AddBlazorWebViewDeveloperTools();
+            builder.Logging.AddDebug();
 #endif
 
             return builder.Build();
